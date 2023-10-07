8bd892bc58b7ba6a8dc692233bd3e8e50c4b2609
sadiquecode <muhammadsadique116@gmail.com>
2023-10-07 22:01:43 +0500

    code

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const select = document.getElementById('select');
-const arrowIcon = document.querySelector('.arrow-icon');
-
-select.addEventListener('click', () => {
-    const customSelect = select.parentElement;
-    customSelect.classList.toggle('expanded');
-    arrowIcon.classList.toggle('expanded');
-});
