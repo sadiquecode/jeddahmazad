230eaab626216956fe6cd237c1e8ec4a6416d5f4
sadiquecode <muhammadsadique116@gmail.com>
2023-10-11 23:06:35 +0500

    codesss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,47 @@
 var jq = jQuery.noConflict();
 
 jq(document).ready(function () {
-    jq('.slide').slick({
+jq('.auctionslide__cards').slick({
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+});
+
+// var jq = jQuery.noConflict();
+
+jq(document).ready(function () {
+    jq('.slideimgs').slick({
         infinite: true,
         autoplay: true,
         speed: 300
